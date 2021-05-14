ac04ccd761f8166494d51b242fcdf638f47ebacb
Aduda-Boaz <adudaboaz@gmail.com>
2021-05-14 15:37:41 +0300

    Modify the game logic files

--- bin/main.rb
+++ bin/main.rb
@@ -1,70 +1,7 @@
 #!/usr/bin/env ruby
-
-require_relative '../lib/selection'
-require_relative '../lib/player'
-
-begin
-  module GameText
-    WELCOME_MESSAGE = "Welcome to Boaz and Joshua's Tic Tac Toe Game"
-    INSTRUCTIONS = ['The game is played on a 3 by 3 sqaure grid by two players',
-                    'At the start of the game players select thier position as X (Player 1) or O (Player 2)',
-                    'Players take turns putting their marks in empty squares by inputting a number corresponding to the square in which they want to play',
-                    'The first player to get 3 marks in a row (up, down, across, or diagonally) is the winner',
-                    'When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie.']
-    EXIT_MESSAGE = ['Thank you for playing our game', 'Have a good day!']
-    INTERRUPT_MESSAGE = ["\nWe're sorry to see you go.\nExiting..."]
-  end
-
-  board_interface = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-  def display_board(board_interface)
-    puts "+---+---+---+"
-    puts "| #{board_interface[0]} | #{board_interface[1]} | #{board_interface[2]} |"
-    puts "+---+---+---+"
-    puts "| #{board_interface[3]} | #{board_interface[4]} | #{board_interface[5]} |"
-    puts "+---+---+---+"
-    puts "| #{board_interface[6]} | #{board_interface[7]} | #{board_interface[8]} |"
-    puts "+---+---+---+"
-  end
-
-  display_board(board_interface)
-
-  module GameUtils
-    def self.clear_screen
-      system('clear') || system('cls')
-    end
-
-    def self.word_wrap(words, indent = 0)
-      character_array = words.split('')
-      new_character_array = []
-      reset_counter = 0
-
-      terminal_width = `tput cols`.to_i
-      maximum_length = terminal_width - (terminal_width * 0.15)
-
-      character_array.each do |character|
-        if character == "\n"
-          reset_counter = 0
-        elsif reset_counter >= maximum_length && character == ' '
-          if indent.positive?
-            new_character_array << "\n"
-            (0...(indent - 1)).each do |_i|
-              new_character_array << ' '
-              reset_counter += 1
-            end
-            reset_counter = 0
-          else
-            character = "\n"
-            reset_counter = 0
-          end
-        end
-        new_character_array << character
-        reset_counter += 1
-      end
-
-      new_character_array.join.to_s
-    end
-  end
+require_relative '../lib/modules/gametext'
+require_relative '../lib/modules/gameutils'
+require_relative '../lib/game'
 
 begin
   def print_welcome_message
--- lib/player.rb
+++ lib/player.rb
@@ -1,18 +1,9 @@
 class Player
-<<<<<<< HEAD
-  attr_reader :name
-  attr_accessor :symbol
-=======
   attr_reader :name, :symbol, :moves
->>>>>>> 4cb7dc6945afab47ea7960c8aeaad72e872a2f7b
 
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
-<<<<<<< HEAD
-  end
-end
-=======
     @moves = []
   end
 
@@ -20,4 +11,3 @@ end
     moves << position
   end
 end
->>>>>>> 4cb7dc6945afab47ea7960c8aeaad72e872a2f7b
